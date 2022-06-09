4483b282fbf14d922ec3178af1d13ee77d4b4e6b
Guilherme Melo <jguilhermefmelo21@gmail.com>
2022-06-09 20:39:57 -0300

    migration e config env

--- src/infrastructure/database/config/config.js
+++ src/infrastructure/database/config/config.js
@@ -1,11 +1,12 @@
 require('dotenv').config()
 module.exports = {
   "development": {
-    "username": "root",
-    "password": "root",
-    "database": "la_vie_migrations",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
+    "port": process.env.DB_PORT,
+    "dialect": process.env.DB_DIALECT
   },
   "test": {
     "username": "root",
